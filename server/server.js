ad5703494aa26f49a9bbc27585436a5dd44e7cb6
Dragan Roganović <unrealityxi@gmail.com>
2017-03-24 21:44:51 +0100

    Added GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+var {ObjectID} = require("mongodb");
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -21,12 +22,31 @@ app.post('/todos', (req, res) => {
   });
 });
 
-
 app.get("/todos", (req, res) => {
   Todo.find().then((todos)=>{
     res.send({todos});
   }, (e)=>{
     res.status(400).send(e);
+  });
+});
+
+// GET /todos/todo
+
+app.get("/todos/:id", (req, res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)){
+    return res.send("Invalid id");
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo){
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+
+  }).catch((e) => {
+    console.log(e);
   });
 });
 
